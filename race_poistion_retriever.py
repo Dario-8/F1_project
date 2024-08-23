79a43ee5e2d486850cf91737be937c1aac4123db
Dario <dario.dastoli@gmail.com>
2024-08-23 12:32:17 +0200

    Modified race_position_retriever.py to use list comprehension

--- race_poistion_retriever.py
+++ race_poistion_retriever.py
@@ -10,7 +10,7 @@ class RacePositionRetriever():
 
     def retrieve_race_informations(self):
         """Retrieve all the data to compare the two drivers"""
-        filename = 'F1_Project\\lap_times.csv'
+        filename = 'F1_Project\\CSV Data\\lap_times.csv'
         with open(filename) as f:
             reader = csv.reader(f)
 
@@ -29,10 +29,10 @@ class RacePositionRetriever():
 
     def split_laps_positions(self, lap_time_list):
         """Split the number of laps and position for that lap"""
-        laps = []
-        positions = []
-        for lap in lap_time_list:
-            laps.append(lap['lap'])
-            positions.append(lap['position'])
+        laps = [lap['lap'] for lap in lap_time_list]
+        positions = [lap['position'] for lap in lap_time_list]
+        # for lap in lap_time_list:
+        #     laps.append(lap['lap'])
+        #     positions.append(lap['position'])
         return laps, positions
         
