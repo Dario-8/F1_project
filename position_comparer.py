6d80c729b95f12c473bab230754a2beec948614a
Dario <dario.dastoli@gmail.com>
2024-08-26 23:06:11 +0200

    added csv files, added type hints

--- position_comparer.py
+++ position_comparer.py
@@ -1,15 +1,19 @@
 from matplotlib import pyplot as plt
 from race_poistion_retriever import RacePositionRetriever
 
-#racedId 1084 = Austria 2022,
-LEC_rpr = RacePositionRetriever(1084, 844) #driverId 844 = C.Leclerc 
-VER_rpr = RacePositionRetriever(1084, 830) #driverId 830 = M.Verstappen
+# Initialize RacePositionRetriever for each driver
+race_id = 1084  # Austria 2022
+leclerc_driver_id = 844  # C. Leclerc
+verstappen_driver_id = 830  # M. Verstappen
 
-LEC_lap_time_list = LEC_rpr.retrieve_race_informations()
-VER_lap_time_list = VER_rpr.retrieve_race_informations()
+leclerc_rpr = RacePositionRetriever(race_id, leclerc_driver_id)
+verstappen_rpr = RacePositionRetriever(race_id, verstappen_driver_id)
 
-LEC_laps, LEC_positions = LEC_rpr.split_laps_positions(LEC_lap_time_list)
-VER_laps, VER_positions = VER_rpr.split_laps_positions (VER_lap_time_list)
+LEC_lap_time_list = leclerc_rpr.retrieve_race_informations()
+VER_lap_time_list = verstappen_rpr.retrieve_race_informations()
+
+LEC_laps, LEC_positions = leclerc_rpr.split_laps_positions(LEC_lap_time_list)
+VER_laps, VER_positions = verstappen_rpr.split_laps_positions (VER_lap_time_list)
 
 plt.plot(LEC_laps, LEC_positions, linewidth=2, c='red', label='LEC')
 plt.plot(VER_laps, VER_positions, linewidth=2, c='blue', label='VER')
@@ -18,6 +22,7 @@ plt.title("LEC vs VER position by lap Australia 2022", fontsize=18)
 plt.xlabel("Lap", fontsize=14)
 plt.ylabel("Position", fontsize=14)
 plt.legend(loc='best')
+plt.grid(True)
 plt.axis([0,75, 0, 10])
 
 plt.show()
--- race_poistion_retriever.py
+++ race_poistion_retriever.py
@@ -1,33 +1,34 @@
 import csv
+from typing import Dict, List, Tuple
 
 class RacePositionRetriever():
     """A class to retrieve the informations about drivers during a passed race"""
-    def __init__(self, raceId, driverId) -> None:
+    def __init__(self, raceId: int, driverId: int) -> None:
         """Initialize attributes to retrieve the position"""
         self.raceId = str(raceId)
         self.driverId = str(driverId)
         
 
-    def retrieve_race_informations(self):
+    def retrieve_race_informations(self) -> List[Dict[str, int]]:
         """Retrieve all the data to compare the two drivers"""
         filename = 'F1_Project\\CSV Data\\lap_times.csv'
-        with open(filename) as f:
-            reader = csv.reader(f)
+        try:
 
+            with open(filename) as f:
+                reader = csv.reader(f)
 
-            lap_time_list = []
+                lap_time_list = [
+                    {'lap': int(row[2]), 'position': int(row[3])}
+                    for row in reader
+                    if row[0] == self.raceId and row[1] == self.driverId
+                ]
+        except FileNotFoundError:
+            msg = "Sorry, the file " + filename + " does not exist."
+            print(msg)
 
-            for row in reader:
-                if row[0] == self.raceId:
-                    if row[1] == self.driverId:
-                        lap_time_dictionary = {}
-                        lap_time_dictionary['lap'] =  int(row[2])
-                        lap_time_dictionary['position'] =  int(row[3])
-                        lap_time_list.append(lap_time_dictionary)
-        
         return lap_time_list
 
-    def split_laps_positions(self, lap_time_list):
+    def split_laps_positions(self, lap_time_list: list[dict[str, int]]) -> Tuple[List[int], List[int]]:
         """Split the number of laps and position for that lap"""
         laps = [lap['lap'] for lap in lap_time_list]
         positions = [lap['position'] for lap in lap_time_list]
